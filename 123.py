88d219ec08ed66604146eca673e294a7abe60f52
Voviman <60238544+Voviman@users.noreply.github.com>
2020-01-24 02:12:28 +0500

    Update 123.py

--- 123.py
+++ 123.py
@@ -1,46 +1,48 @@
-#TGbot
+# TGbot
 
 import pyowm
 import telebot
 
-owm = pyowm.OWM('5edfb276a6e154e914b96ac9bf1cf2a0', language = "ru")  # You MUST provide a valid API key
+owm = pyowm.OWM('5edfb276a6e154e914b96ac9bf1cf2a0', language="ru")
 bot = telebot.TeleBot("986149616:AAGe3UbECorAMKDy7yCD_LL2cP106cEGH5E")
 
-#обычеая отправка сообщений -->
+# обычеая отправка сообщений -->
 
-#bot.send_mesage(message.chat.id, message.text)
+# bot.send_mesage(message.chat.id, message.text)
+
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-	bot.reply_to(message, "Привет! чтобы узнать погоду напиши название города/страны")
+    bot.reply_to(message, "Привет! чтобы узнать погоду напиши название города")
+
 
 @bot.message_handler(commands=['help'])
 def send_welcome(message):
-	bot.reply_to(message, "Чтобы узнать погоду напиши название города/страны")
+    bot.reply_to(message, "Чтобы узнать погоду напиши название города/страны")
+
 
 @bot.message_handler(content_types=['text'])
 def send_echo(message):
-	observation = owm.weather_at_place(message.text)
-	w = observation.get_weather()
-	temp = w.get_temperature('celsius')["temp"]
+    observation = owm.weather_at_place(message.text)
+    w = observation.get_weather()
+    temp = w.get_temperature('celsius')["temp"]
 
-	answer = "В городе " + message.text + " сейчас " + w.get_detailed_status() + "\n"
-	answer += "Температура сейчас " + str(temp) + "\n\n"
+    answer = "В городе " + message.text + " сейчас " + w.get_detailed_status() + "\n"
+    answer += "Температура сейчас " + str(temp) + "\n\n"
 
-	if temp < 10:
-		answer += "На улице очень холодно. Желательно одеться потеплее."
-	elif temp < 20:
-		answer += "На улице достаточно холодно. Незабудь куртку."
-	else:
-		answer += "Погода заебумба, хоть в трусах гуляй."
+    if temp < 10:
+        answer += "На улице очень холодно. Желательно одеться потеплее."
+    elif temp < 20:
+        answer += "На улице достаточно холодно. Незабудь куртку."
+    else:
+        answer += "Погода заебумба, хоть в трусах гуляй."
 
-	bot.send_message(message.chat.id, answer)
-
-
-#@bot.message_handler(content_types=['text'])
-#def handle_message(message):
-	#bot.reply_to(message, "----")
+    bot.send_message(message.chat.id, answer)
 
 
+# @bot.message_handler(content_types=['text'])
+# def handle_message(message):
+    # bot.reply_to(message, "----")
 
-bot.polling( none_stop = True )
+
+bot.polling(none_stop=True)
